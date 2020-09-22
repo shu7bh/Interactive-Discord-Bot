b18c21ab601f74acc3abfa2933e1c3676e5ecc2f
shu7bh <shubh.agarwal@students.iiit.ac.in>
2020-09-22 13:16:46 +0300

    Added aliases comments

--- bot.py
+++ bot.py
@@ -10,8 +10,7 @@ def checkName(author):
 
     with open("realNames.txt", "r") as f:
         for person in f:
-            _id, name = tuple(person.split(" = ", 1))
-            if str(author.id) == _id:
+            if str(author.id) == tuple(person.split(" = ", 1))[0]:
                 return True
     return False
 
@@ -41,8 +40,7 @@ def removeName(userId):
     with open("realNames.txt", 'r') as f:
         with open("temp.txt", 'w') as fout:
             for person in f:
-                _id, name = person.split(' = ', 1)
-                if (str(userId)) == _id:
+                if (str(userId)) == person.split(' = ', 1)[0]:
                     isPresent = True
                 else:
                     fout.write(person)
@@ -53,6 +51,11 @@ def removeName(userId):
     
     return "your identity doesn't exist"
 
+
+
+def reidentify(_id,name):
+    removeName(_id)
+    selfIdentify(_id,name)
 
 
 @bot.event
@@ -85,7 +88,7 @@ async def ping(context):
 
 @bot.command(aliases = ['8ball'])
 async def _8ball(context, *, q):
-    'A fun section where the bot predicts answers'
+    ' Aliases = 8ball; A fun section where the bot predicts answers'
 
     res = list(open('8ballResponses.txt', 'r').readlines())
     await context.send(f'Question: {q}\nAnswer: {random.choice(res)}')
@@ -93,7 +96,7 @@ async def _8ball(context, *, q):
 
 @bot.command(aliases = ['remove'])
 async def clear(ctx, amt = 10):
-    'To remove some lines from the history'
+    ' Aliases = remove; To remove some lines from the history'
 
     await ctx.channel.purge(limit=amt+1)
 
@@ -103,8 +106,9 @@ async def selfidentify(ctx, *, name):
     'You can add your identity from here'
 
     author = ctx.author
-    if (checkName(author)):
-        await ctx.send(f'{author} your current identity is {findName(author)}To change your identity, remove your current identity and try again')
+    if checkName(author):
+        reidentify(author.id, name)
+        await ctx.send(f'{author} your identity has been updated to {name}')
         return
     
     selfIdentify(author.id ,name)
@@ -113,7 +117,7 @@ async def selfidentify(ctx, *, name):
 
 @bot.command(aliases = ['id'])
 async def identify(ctx):
-    'You can find any users identity from here if they have added it'
+    ' Aliases = id; You can find any users identity from here if they have added it'
 
     ans = ''
     for person in ctx.message.mentions:
@@ -123,15 +127,9 @@ async def identify(ctx):
 
 @bot.command(aliases = ['removeid', 'rid'])
 async def removeidentity(ctx):
-    'To remove your current identity'
+    ' Aliases = removeid, rid; To remove your current identity'
 
     _id = ctx.author.id
     await ctx.send(f'{ctx.author} {removeName(_id)}')
 
-
-# @bot.command()
-# async def sdentify(ctx, *, name):
-#     'To self identify a person'
-#     await ctx.send(f'{ctx.author}, you are identified as {name}')
-#
 bot.run(open('token.txt', 'r').readline())
